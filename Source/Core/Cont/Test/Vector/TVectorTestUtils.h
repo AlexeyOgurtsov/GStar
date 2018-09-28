b8e3f576a879ca64b02c2fa611c9d40454491e76
AlexeyOgurtsov <alexey_eng@mail.ru>
2018-09-28 13:43:01 +0600

    TVector unit test splitted into header files

---
+++ Source/Core/Cont/Test/Vector/TVectorTestUtils.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Core/Cont/TVector.h"
+#include <boost/test/included/unit_test.hpp>
+#include <memory> // std::unique_ptr
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <string>
+
+
+namespace
+{
+
+BOOST_AUTO_TEST_SUITE(Core)
+BOOST_AUTO_TEST_SUITE(Container)
+BOOST_AUTO_TEST_SUITE(TVectorTestSuite)
+
+
+BOOST_AUTO_TEST_SUITE_END() // TVectorTestSuite
+BOOST_AUTO_TEST_SUITE_END() // Container
+BOOST_AUTO_TEST_SUITE_END() // Core
+
+} 
